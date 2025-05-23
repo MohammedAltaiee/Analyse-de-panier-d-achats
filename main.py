69b17e62704026b016630b94b71b5853793d53d4
MohammedAltaiee <altaiem888@gmail.com>
2025-05-23 12:52:02 +0000

    rasdfgsfgs

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ app = Flask(__name__)
 test_produits = Produit.ProduitEntite([])
 # test_produits.ajouter_produits_depuis_csv("./data/produits.csv")
 
-global prods  # Declare produits as global to modify it
-prods = Produit.ProduitEntite([(0, "Le_Savoir", 0)])
+# global prods  # Declare produits as global to modify it
 # prods = Produit.ProduitEntite([(0, "Le_Savoir", 0)])
-# produits.ajouter_produits_depuis_csv("./data/produits.csv")
-prods.ajouter_produit_tuple(id_produit=0, nom_produit="Le_Savoir", prix_produit=0)
-print(prods)
-logger.debug("Debut script, Debug message: {prods}")
+# # prods = Produit.ProduitEntite([(0, "Le_Savoir", 0)])
+# # produits.ajouter_produits_depuis_csv("./data/produits.csv")
+# prods.ajouter_produit_tuple(id_produit=0, nom_produit="Le_Savoir", prix_produit=0)
+# print(prods)
+# logger.debug("Debut script, Debug message: {prods}")
 
 
 @app.route('/bonjour', methods=['GET'])
