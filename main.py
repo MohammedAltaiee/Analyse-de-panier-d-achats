650556c150e830dabe5c938bdbf0b3d9b249efc9
MohammedAltaiee <altaiem888@gmail.com>
2025-05-23 12:18:45 +0000

    test1112

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ global prods  # Declare produits as global to modify it
 prods = Produit.ProduitEntite([])
 # produits.ajouter_produits_depuis_csv("./data/produits.csv")
 prods.ajouter_produit_tuple(id_produit=0, nom_produit="Le_Savoir", prix_produit=0)
+print(prods)
 
 
 @app.route('/bonjour', methods=['GET'])
